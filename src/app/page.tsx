853d82f492b593b5f86144d638087fc6ad5a045a
dnjeh <ehdnj.001@gmail.com>
2025-01-23 00:32:47 +0900

    feat: added a little response to title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-
 export default function Home() {
   return (
-    <div className="mx-auto max-w-screen-2xl mt-24">
-      <span className="text-6xl font-semibold font-pretendard">
-      <span>오, </span>
-      <span className="text-purple-500">라일락&nbsp;</span>
-      <span>꽃이 피는 날&nbsp;</span>
-      <span>안녕</span>
-      </span>
+    <div className="mx-auto max-w-screen-2xl lg:mt-24 mt-12 flex flex-col items-center lg:px-24 md:px-16 px-12">
+      <h2 className="text-4xl md:text-6xl font-semibold font-pretendard flex lg:flex-row flex-col w-full lg:items-center items-end">
+        <div className="self-start">
+          <span>오,&nbsp;</span>
+          <span className="text-purple-500">라일락</span>
+        </div>
+        <div className="md:block lg:opacity-100 md:opacity-0 hidden bg-gradient-to-r from-purple-500 to-slate-950 flex-1 h-1 mx-4" />
+        <div>꽃이 피는 날</div>
+        <div>&nbsp;안녕</div>
+      </h2>
     </div>
   );
 }
