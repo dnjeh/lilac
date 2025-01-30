1ff703475548f75c76ed19cd70bdc123ba9a8e2f
dnjeh <ehdnj.001@gmail.com>
2025-01-30 22:25:47 +0900

    final

--- src/app/_components/line.tsx
+++ src/app/_components/line.tsx
@@ -6,7 +6,7 @@ interface LineProps {
   id?:string
 }
 const Line = ({id}:LineProps) => {
-  const { theme, setTheme } = useTheme();
+  const { theme } = useTheme();
   return <div className="w-full flex items-center opacity-10 my-4 sm:my-2 md:my-10 lg:my-12" id={id}>
     <hr className="flex-1 border-t-1 border-t-slate-950 dark:border-t-slate-50"/>
     {
