1ba93f0af4f9c32316be0a84d6455ce03afb2648
dnjeh <ehdnj.001@gmail.com>
2025-03-23 02:53:25 +0900

    c

--- src/app/_components/certificateHeader.tsx
+++ src/app/_components/certificateHeader.tsx
@@ -1,7 +1,7 @@
 const CertificateHeader = () => {
   return (
     <h3 className="text-3xl sm:text-4xl lg:text-5xl font-bold flex w-full">
-      자격증과 수상목록
+      자격증과 상장들
       <span className="text-purple-500 dark:text-yellow-300">.</span>
     </h3>
   );
--- src/app/_components/certificateWrap.tsx
+++ src/app/_components/certificateWrap.tsx
@@ -5,6 +5,20 @@ const CertificateWrap = () => {
   return (
     <Section id="certificate">
       <CertificateHeader />
+      <div className="flex">
+        <ul className="flex-1">
+
+        </ul>
+      <ul className="h-[calc(100dvh-14rem)] w-[calc((100dvh-14rem)/27*40+4rem)] m-8 p-8 border-2 rounded-3xl mr-0">
+        <ul>
+        {
+          ["정보처리기능사", "웹디자인기능사", "정보기기운용기능사", "전자기기기능사"].map((ai, i)=>(
+            <li key={i}>{ai}</li>
+          ))
+        }
+        </ul>
+
+      </ul></div>
     </Section>
   );
 };
